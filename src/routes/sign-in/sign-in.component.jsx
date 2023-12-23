530db8e1b215ec9e160f55ce8eff4347bd771561
Reiner Knudsen <reinerknudsen@me.com>
2023-12-23 17:42:59 +0000

    Signin added

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,17 @@
-function SignIn() {
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase.utils';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    // from the total response all we need is the user object
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
   return (
     <div>
       <h1>Come on in!</h1>
+      <button onClick={logGoogleUser}>Sign in with Google popup</button>
     </div>
   );
-}
+};
 
 export default SignIn;
