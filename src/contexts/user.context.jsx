cd3954c94655fb53ac8d36f4eea3a5afe26d3478
Reiner Knudsen <reinerknudsen@me.com>
2024-01-01 22:42:31 +0100

    Implemented Reducer for user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,29 +1,47 @@
-import { createContext, useState, useEffect } from 'react';
-
+import { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase.utils';
 
-// the context is the actual VALUE we want to exit from other components
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
-// the Provider is the actual COMPONENT we can use to wrap those child components
-// we want to provide the context to
-// the Provider provides his state objects to all his child components
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer !`);
+  }
+};
+
+const INITIAL_USER_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_USER_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
-    // this initiates the Listener as soon as the component mounts
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
-        // in the follwoing function we verify if the user already exsists
         createUserDocumentFromAuth(user);
       }
-      // if the user logged out, 'user' will be null
-      // if a user signed in, 'user' will be the user object
       setCurrentUser(user);
     });
     return unsubscribe;
